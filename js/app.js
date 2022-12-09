b98601142d02032b5d24ab29aa65aa676a0964b7
Andrew-Neely-82 <andrew.neely82@gmail.com>
2022-12-09 16:22:05 -0600

    updated

--- js/app.js
+++ js/app.js
@@ -1,40 +1,58 @@
 (() => {
   const url = "https://codeup-json-server.glitch.me/movies";
-
-  const html = {
-    divO: `<div class="movie">`,
-    titleO: `<h1 class="title">`,
-    titleC: `</h1>`,
-    directorO: `<span class="director">`,
-    directorC: `</span>`,
-    idO: `<span class="id">`,
-    idC: `</span>`,
-    genreO: `<span class="genre">`,
-    genreC: `</span>`,
-    ratingO: `<span class="rating">`,
-    ratingC: `</span>`,
-    buttons: `<div class="movie-buttons"><button class="edit-movie">Edit</button><button class="delete-movie">Delete</button></div>`,
-    divC: `</div>`,
-  };
-
-  // TODO: When the initial AJAX request comes back, remove the "loading..." message and replace it with HTML generated from the json response your code receives
-
-  $(document).ready(function () {
+  const moviePosterUrl = `http://www.omdbapi.com/?apikey=${movieKey}&`;
+  $(document).ready(() => {
+    $(`.navbar-brand`).html(icon.popcorn);
     $(`.page-load`).html(`Loading`);
-    $.get(url, function (data) {
-      console.log(data);
-      // make is to where the movies image is the shown from the name of the movie
-
-      data.forEach(function (movie) {
-        $(`.container-movies`).append(`${html.divO}${html.titleO}${movie.title}${html.titleC}${html.directorO}${movie.director}${html.directorC}${html.idO}${movie.id}${html.idC}${html.genreO}${movie.genre}${html.genreC}${html.ratingO}${movie.rating}${html.ratingC}${html.buttons}${html.divC}`);
+    $(`.container-add`).hide();
+    $.get(url, (data) => {
+      data.forEach((movie) => {
+        $.get(`${moviePosterUrl}t=${movie.title}`, (data) => {
+          const movieRating = () => {
+            if (movie.rating === 1) {
+              movie.rating = rating.one;
+            } else if (movie.rating === 2) {
+              movie.rating = rating.two;
+            } else if (movie.rating === 3) {
+              movie.rating = rating.three;
+            } else if (movie.rating === 4) {
+              movie.rating = rating.four;
+            } else if (movie.rating === 5) {
+              movie.rating = rating.five;
+            } else {
+              movie.rating = `N/A`;
+            }
+          };
+          const directorInfo = () => {
+            if (movie.director === undefined) {
+              movie.director = `Needs updated`;
+            }
+          };
+          const genreInfo = () => {
+            if (movie.genre === undefined) {
+              movie.genre = `Needs updated`;
+            }
+          };
+          const posterInfo = () => {
+            if (data.Response === `False`) {
+              data.Poster = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjQk5myChutYz0rHuGWaDlhcGMzOCABLt_LA&usqp=CAU`;
+            }
+          };
+          movieRating();
+          directorInfo();
+          genreInfo();
+          posterInfo();
+          $(`.container-movies`).append(`${html.divO}${html.posterO}${data.Poster}${html.posterC}${html.titleO}${movie.title}${html.titleC}${html.directorO}${movie.director}${html.directorC}${html.idO}${movie.id}${html.idC}${html.genreO}${movie.genre}${html.genreC}${html.ratingO}${movie.rating}${html.ratingC}${html.buttons}${html.divC}`);
+          $(`.poster`).click(() => {
+            window.open(`${data.Poster}`, `_blank`);
+          });
+        });
       });
       $(`.page-load`).html(`My First Movies App`);
+      $(`.container-add`).show();
       // * Allow users to add new movies
-
       // * Create a form for adding a new movie that has fields for the movie's title and rating
-
       // TODO: When the form is submitted, the page should not reload / refresh, instead, your javascript should make a POST request to /movies with the information the user put into the form
-
       $(`#submit-movie`).submit(function (event) {
         event.preventDefault();
         const newMovie = {
@@ -46,12 +64,23 @@
           $(`.container-movies`).append(`${html.divO}${html.titleO}${data.title}${html.titleC}${html.directorO}${data.director}${html.directorC}${html.idO}${data.id}${html.idC}${html.genreO}${data.genre}${html.genreC}${html.ratingO}${data.rating}${html.ratingC}${html.divC}`);
         });
       });
+      // * Allow users to delete movies
+      $(`#delete`).click(() => {
+        const id = $(this).data(`id`);
+        $.ajax({
+          url: `${url}/${id}`,
+          type: `DELETE`,
+          success: (result) => {
+            console.log(`Deleted movie with id ${id}`);
+          },
+        });
+      });
+    });
+  });
+  // TODO: add movie
+  $(document).ready(() => {
+    $(`#add-movie`).click(() => {
+      $(`.container-add`).toggle();
     });
   });
 })();
-
-// ! {DELETE MOVIE} modal use "toggle between modals"
-
-// document.getElementById(`submit-movie`).addEventListener(`click`, function (event) {});
-
-// $(`#submit-movie`).hover((event) => {});
